5186e04d4917c569ec9b89731f72816636163f03
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-05-17 00:52:05 +0200

    Silence Warning of unused method argument

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn parse_to_winres_version_as_result(semver_str: &str) -> Result<u64, &'static str> {
+pub fn parse_to_winres_version_as_result(_semver_str: &str) -> Result<u64, &'static str> {
     Err("Not implemented")
 }
 
