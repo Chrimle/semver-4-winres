07ff1055f9f1132bb0ecbd01a54d12bbba21bf48
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-05-17 23:23:23 +0200

    Implement `parse_to_winres_version_as_result`-function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-pub fn parse_to_winres_version_as_result(_semver_str: &str) -> Result<u64, &'static str> {
+pub fn parse_to_winres_version_as_result(semver_str: &str) -> Result<u64, &'static str> {
+    let match_iterator = semver_str.matches("^\\d\\.\\d\\.\\d$");
+
+    if match_iterator.count() == 0 {
+        return Err("Invalid Format of semver_str!");
+    }
+
     Err("Not implemented")
 }
 
@@ -7,7 +13,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert!(parse_to_winres_version_as_result("ignored").is_err());
+    fn test_empty_str_fails() {
+        assert!(parse_to_winres_version_as_result("").is_err());
+    }
+
+    #[test]
+    fn test_invalid_str_fails() {
+        assert!(parse_to_winres_version_as_result("invalid").is_err());
+    }
+
+    #[test]
+    fn test_incomplete_str_fails() {
+        assert!(parse_to_winres_version_as_result("1.2").is_err());
     }
 }
