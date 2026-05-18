bd514724e340bc320e2b9fb31f6e2824c6290944
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-05-18 18:32:43 +0200

    Implement Version Parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use regex;
 use regex::Regex;
 
+#[derive(Debug)]
 pub enum SemVerError {
     InvalidFormat(String),
     MissingVersion(String),
@@ -15,28 +16,30 @@ pub fn parse_to_winres_version_as_result(semver_str: &str) -> Result<u64, SemVer
         .captures(semver_str)
         .ok_or_else(|| SemVerError::InvalidFormat(semver_str.to_string()))?;
 
-    let _major: u64 = captures
+    let _major: u16 = captures
         .name("major")
         .ok_or_else(|| SemVerError::MissingVersion("major".to_string()))?
         .as_str()
-        .parse::<u64>()
+        .parse::<u16>()
         .map_err(|_| SemVerError::InvalidVersion("major".to_string()))?;
 
-    let _minor: u64 = captures
+    let _minor: u16 = captures
         .name("minor")
         .ok_or_else(|| SemVerError::MissingVersion("minor".to_string()))?
         .as_str()
-        .parse::<u64>()
+        .parse::<u16>()
         .map_err(|_| SemVerError::InvalidVersion("minor".to_string()))?;
 
-    let _patch: u64 = captures
+    let _patch: u16 = captures
         .name("patch")
         .ok_or_else(|| SemVerError::MissingVersion("patch".to_string()))?
         .as_str()
-        .parse::<u64>()
+        .parse::<u16>()
         .map_err(|_| SemVerError::InvalidVersion("patch".to_string()))?;
 
-    return Ok(1);
+    let winres_version: u64 = u64::from(_major) << 48 | u64::from(_minor) << 32 | u64::from(_patch) << 16;
+
+    Ok(winres_version)
 }
 
 #[cfg(test)]
@@ -77,6 +80,36 @@ mod tests {
 
     #[test]
     fn test_valid_str_passes() {
-        assert!(parse_to_winres_version_as_result("1.2.3").is_ok_and(|x| x == 1));
+        assert_eq!(281483566841856, parse_to_winres_version_as_result("1.2.3").unwrap());
     }
+
+    #[test]
+    fn test_maximum_valid_str_passes() {
+        assert_eq!(18446744073709486080, parse_to_winres_version_as_result("65535.65535.65535").unwrap());
+    }
+
+    #[test]
+    fn test_u16_patch_overflow_str_fails() {
+        assert!(matches!(
+            parse_to_winres_version_as_result("0.0.65536"),
+            Err(SemVerError::InvalidVersion(_))
+        ));
+    }
+
+    #[test]
+    fn test_u16_minor_overflow_str_fails() {
+        assert!(matches!(
+            parse_to_winres_version_as_result("0.65536.0"),
+            Err(SemVerError::InvalidVersion(_))
+        ));
+    }
+
+    #[test]
+    fn test_u16_major_overflow_str_fails() {
+        assert!(matches!(
+            parse_to_winres_version_as_result("65536.0.0"),
+            Err(SemVerError::InvalidVersion(_))
+        ));
+    }
+
 }
